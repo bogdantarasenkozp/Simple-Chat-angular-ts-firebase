5a426aa04f83cfaf9e0d3078cade775ac88a4f7f
bogdan <bogdanfrontend@gmail.com>
2017-04-23 20:38:22 +0000

    angular 4 simple firebase chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyA-nYRU0EOxRrPAnCmHLlT3WLJxSogSu9k",
+    authDomain: "testchat-9a18e.firebaseapp.com",
+    databaseURL: "https://testchat-9a18e.firebaseio.com",
+    projectId: "testchat-9a18e",
+    storageBucket: "testchat-9a18e.appspot.com",
+    messagingSenderId: "980735192931"
+};
+
 
 @NgModule({
   declarations: [
@@ -12,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
