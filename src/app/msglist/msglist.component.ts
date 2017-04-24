50e9f5a02f855637694a70c3527e6433eebdfeaa
bogdantarasenk <btarasenko@spro.io>
2017-04-24 12:59:47 +0300

    fix

--- src/app/msglist/msglist.component.ts
+++ src/app/msglist/msglist.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'msgListComponent',
-  templateUrl: './msglist.component.html'
+  templateUrl: './msglist.component.html',
+  styleUrls: ['./msglist.component.css']
 })
 
 export class msgListComponent {
